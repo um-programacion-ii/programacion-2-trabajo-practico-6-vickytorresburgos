0735e51ea727c70e950a209058e8abc4e665a53a
vickytorresburgos <mvb.alumno.um.edu.ar>
2025-10-11 22:44:17 -0300

    add entidades jpa y relaciones

---
+++ data-service/src/main/java/com/example/dataservice/entity/Categoria.java
@@ -0,0 +1,29 @@
+package com.example.dataservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "categorias")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100, unique = true)
+    private String nombre;
+
+    @Column(length = 500)
+    private String descripcion;
+
+    @OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Producto> productos = new ArrayList<>();
+}
